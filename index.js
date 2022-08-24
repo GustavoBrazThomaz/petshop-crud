c79375bafd59478ce5efa28a5327018d2f152b45
GustavoBrazThomaz <gustavo.brazthomaz@gmail.com>
2022-08-24 18:49:15 -0300

    Cors Error Fixed

--- index.js
+++ index.js
@@ -22,5 +22,13 @@ app.use(
     }),
 );
 
+app.use(function (req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    next();
+});
+
 const Router = require('./api/src/router/router');
 app.use('/api/customer', Router);
