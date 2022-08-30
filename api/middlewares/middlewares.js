9e1825f04c571c6ccce063ae2778afa9fb5c6dd8
GustavoBrazThomaz <gustavo.brazthomaz@gmail.com>
2022-08-30 19:40:10 -0300

    Cors Auth error Resolvido

--- api/middlewares/middlewares.js
+++ api/middlewares/middlewares.js
@@ -1,34 +1,20 @@
 require("dotenv").config();
-const jwt = require('jsonwebtoken')
+const jwt = require("jsonwebtoken");
 
 exports.checkToken = (req, res, next) => {
-    const authHeader = req.headers['authorization']
-    const token = authHeader && authHeader.split(" ")[1]
+  const authHeader = req.headers["authorization"];
+  const token = authHeader && authHeader.split(" ")[1];
 
-    if(!token){
-        return res.status(401).json({msg: 'Acesso negado!'})
-    }
-    try{
-        const secret = process.env.SECRET
-
-        jwt.verify(token, secret)
-        next()
-
-    }catch(error){
-        res.status(400).json({msg: 'Token inválido'})
-    }
-}
-
-exports.CORS = function (req, res, next) {
-    res.setHeader("Access-Control-Allow-Origin", "*");
-    res.setHeader(
-      "Access-Control-Allow-Methods",
-      "GET, POST, OPTIONS, PUT, PATCH, DELETE"
-    );
-    res.setHeader(
-      "Access-Control-Allow-Headers",
-      "X-Requested-With,content-type"
-    );
-    res.setHeader("Access-Control-Allow-Credentials", true);
-    next();
+  if (!token) {
+    return res.status(401).json({ msg: "Acesso negado!" });
   }
+  try {
+    const secret = process.env.SECRET;
+
+    jwt.verify(token, secret);
+    next();
+  } catch (error) {
+    res.status(400).json({ msg: "Token inválido" });
+  }
+};
+
--- index.js
+++ index.js
@@ -25,8 +25,13 @@ app.use(
   })
 );
 
-const {CORS} = require('./api/middlewares/middlewares')
-app.use(CORS);
+//const {CORS} = require('./api/middlewares/middlewares')
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT,DELETE");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+  next();
+});
 
 const Router = require("./api/src/router/router");
 app.use("/api", Router);
